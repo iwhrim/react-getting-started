698fe6950a086d2a71a3acb29734a9013d9ba661
Gustavo Teixeira <iwhrim@gmail.com>
2021-02-19 20:39:37 -0300

    adicionado evento para mudar o valor do item da lista dinamicamente

---
+++ todo/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Todo from './Todo';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Todo />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
